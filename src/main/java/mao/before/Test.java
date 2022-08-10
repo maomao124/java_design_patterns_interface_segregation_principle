5bb45ba647e98c835e0449a3e7ef2bc714b88ee8
mao <1296193245@qq.com>
2022-08-10 21:49:15 +0800

    java_design_patterns_interface_segregation_principle

---
+++ src/main/java/mao/before/Test.java
@@ -0,0 +1,30 @@
+package mao.before;
+
+
+/**
+ * Project name(项目名称)：java设计模式_接口隔离原则
+ * Package(包名): mao.before
+ * Class(类名): Test
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/10
+ * Time(创建时间)： 21:48
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Impl1 impl1 = new Impl1();
+        impl1.a();
+        impl1.b();
+        impl1.c();
+        Impl2 impl2 = new Impl2();
+        impl2.a();
+        impl2.b();
+        impl2.c();
+    }
+}
